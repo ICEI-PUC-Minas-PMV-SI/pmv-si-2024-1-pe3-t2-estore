c4514b052edacff682ac7daf9ea1ba431000cc0d
pedro <pedrohenriquenunesalves067@gmail.com>
2024-03-15 19:34:23 -0300

    feat: 404 error page

--- frontend/src/components/Error404.jsx
+++ frontend/src/components/Error404.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import ErroSVG from "../assets/404.svg";
+import "../css/Error404.css";
+import { Link } from "react-router-dom";
 
 const Error404 = () => {
-  return <div>Error404</div>;
+  return (
+    <div className="img_container">
+      <img className="img" src={ErroSVG} alt="Erro 404" />
+      <p className="p">
+        Você achou uma página que não existe. <br /> Deseja voltar para a <Link to="/">tela principal</Link>?
+      </p>
+    </div>
+  );
 };
 
 export default Error404;

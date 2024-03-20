13cdc73569d586b715c1f03016f9d6051bf17223
pedro <pedrohenriquenunesalves067@gmail.com>
2024-03-19 22:32:05 -0300

    feat: adding initial register screen

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import RegisterImg from "../assets/register.svg";
+import "../css/register.css";
 
-const Register = () => {
-  return <div>Register</div>;
+export const Register = () => {
+  return (
+    <div className="container-register">
+      <div className="container-register-form">
+        <form action="" className="form-register">
+          <h1>Registre-se aqui!</h1>
+          <div className="inputs-register">
+            <div className="textfield-register">
+              <label htmlFor="">Email:</label>
+              <input type="text" name="email" id="email" placeholder="Seu e-mail" />
+            </div>
+            <div className="textfield-register">
+              <label htmlFor="">Senha:</label>
+              <input type="password" name="senha" id="senha" placeholder="Sua senha" />
+            </div>
+            <div className="submit-register">
+              <input type="submit" value="Registrar" />
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <aside className="aside-register">
+        <img className="image-register" src={RegisterImg} alt="Erro 404" />
+      </aside>
+    </div>
+  );
 };
 
 export default Register;

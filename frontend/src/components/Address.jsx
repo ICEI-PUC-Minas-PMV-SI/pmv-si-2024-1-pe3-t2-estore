971c1b6d1156ef37d92693e63b3691a3bcc63765
PedroHNunesAlves <pedrohenriquenunesalves067@gmail.com>
2024-04-15 21:27:28 -0300

    feat: adding table enderecos + address with viaCep API and some css fixs

--- frontend/src/components/Address.jsx
+++ frontend/src/components/Address.jsx
@@ -1,25 +1,93 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../css/Address.css";
+import axios from "axios";
 
 const Address = () => {
+  const [cep, setCEP] = useState("");
+  const [address, setAddress] = useState("");
+  const [info, setInfo] = useState({
+    CEP: "",
+    RUA: "",
+    BAIRRO: "",
+    CIDADE: "",
+    COMPLEMENTO: "",
+    NUMERO: "",
+    DESCRICAO: "",
+  });
+
+  const handleChange = (e) => {
+    let value = e.target.value;
+    value = value.replace(/\D/g, "");
+    if (value.length > 5) {
+      value = value.replace(/^(\d{5})(\d{0,3})/, "$1-$2");
+    }
+    setCEP(value);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (cep.length === 9) {
+        catchCEP();
+      }
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [cep]);
+
+  const catchCEP = async () => {
+    try {
+      const res = await axios.get(`https://viacep.com.br/ws/${cep.replace(/[^0-9]/g, "")}/json/`);
+      const data = res.data;
+      setAddress(data);
+      setInfo({
+        ...info,
+        CEP: data.cep,
+        RUA: data.logradouro,
+        BAIRRO: data.bairro,
+        CIDADE: data.localidade,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const othersInputs = (e) => {
+    const { name, value } = e.target;
+    setInfo({ ...info, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const req = await axios.post("URLPOST", info);
+      console.log(req);
+    } catch (error) {
+      console.log(error);
+    }
+    console.log(info);
+  };
+
   return (
     <div>
-      <form id="form-address">
-        <div class="input-column1">
+      <form id="form-address" onSubmit={handleSubmit}>
+        <div className="input-column1">
           <label htmlFor="cep">CEP: </label>
-          <input type="number" name="cep" className="css-addr" />
+          <input type="text" name="CEP" className="css-addr" onChange={handleChange} value={cep} maxLength={9} />
           <label htmlFor="rua">Rua: </label>
-          <input type="text" name="rua" className="css-addr" />
+          <input type="text" name="RUA" className="css-addr-block" value={address.logradouro} readOnly />
           <label htmlFor="numero">Número: </label>
-          <input type="number" name="numero" className="css-addr" />
+          <input type="number" name="NUMERO" onChange={othersInputs} className="css-addr" />
           <label htmlFor="complemento">Complemento: </label>
-          <input type="text" name="complemento" className="css-addr" />
+          <input type="text" name="COMPLEMENTO" onChange={othersInputs} className="css-addr" />
         </div>
-        <div class="input-column2">
+        <div className="input-column2">
           <label htmlFor="bairro">Bairro: </label>
-          <input type="text" name="bairro" className="css-addr" />
+          <input type="text" name="BAIRRO" className="css-addr-block" value={address.bairro} readOnly />
           <label htmlFor="cidade">Cidade: </label>
-          <input type="text" name="cidade" className="css-addr" />
+          <input type="text" name="CIDADE" className="css-addr-block" value={address.localidade} readOnly />
+          <label htmlFor="cidade">Descrição: </label>
+          <input type="text" name="DESCRICAO" className="css-addr" onChange={othersInputs} />
           <input type="submit" id="submit-address" value="Atualizar dados" />
         </div>
       </form>
--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 import "../css/Profile.css";
 
 const Profile = () => {
-  const infoUser = "http://localhost:3000/users";
   const [userData, setUserData] = useState({
     EMAIL: "",
     NOME: "",
@@ -13,62 +12,34 @@ const Profile = () => {
   });
 
   useEffect(() => {
-    const fetchInfo = async () => {
-      try {
-        const token = localStorage.getItem("token");
-        const res = await axios.get(infoUser, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        const backendData = res.data;
-        console.log(backendData);
-        setUserData(backendData);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchInfo();
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      setUserData(decodedToken);
+    }
   }, []);
-
-  const setInputs = () => {
-    const emailInput = document.getElementById("email-profile");
-    const nameInput = document.getElementById("nome-profile");
-    const secondNameInput = document.getElementById("sobrenome-profile");
-    const cpfInput = document.getElementById("cpf-profile");
-
-    emailInput.value = userData.EMAIL;
-    nameInput.value = userData.NOME;
-    secondNameInput.value = userData.SOBRENOME;
-    cpfInput.value = userData.CPF;
-  };
-
-  useEffect(() => {
-    setInputs();
-  }, [userData]);
 
   return (
     <div className="profile-container">
       <form id="form-profile">
         <label htmlFor="email-profile">E-mail: </label>
-        <input type="email" name="email-profile" id="email-profile" />
-        <label htmlFor="email-profile">Nome: </label>
-        <input type="email" name="email-profile" id="nome-profile" />
-        <label htmlFor="email-profile">Sobrenome: </label>
-        <input type="email" name="email-profile" id="sobrenome-profile" />
-        <label htmlFor="email-profile">CPF: </label>
-        <input type="email" name="email-profile" id="cpf-profile" />
+        <input type="email" name="EMAIL" value={userData.EMAIL} readOnly className="css-addr-block" />
+        <label htmlFor="nome-profile">Nome: </label>
+        <input type="text" name="NOME" value={userData.NOME} readOnly className="css-addr-block" />
+        <label htmlFor="sobrenome-profile">Sobrenome: </label>
+        <input type="text" name="SOBRENOME" id="sobrenome-profile" />
+        <label htmlFor="cpf-profile">CPF: </label>
+        <input type="text" name="CPF" value={userData.CPF} readOnly className="css-addr-block" />
         <div className="input-profile">
-          <input type="submit" id="submit-profile" value="Atualizar dados" />
+          <input type="submit" value="Alterar dados" />
         </div>
       </form>
       <form id="form-password-profile">
         <h1>Deseja trocar sua senha?</h1>
-        <label htmlFor="email-profile">Nova senha:</label>
-        <input type="password" name="email-profile" id="senha-profile" required />
-        <label htmlFor="email-profile">Confirme sua nova senha:</label>
-        <input type="password" name="email-profile" id="senha2-profile" required />
+        <label htmlFor="senha-profile">Nova senha:</label>
+        <input type="password" name="SENHA" id="senha-profile" required />
+        <label htmlFor="senha2-profile">Confirme sua nova senha:</label>
+        <input type="password" name="CONFIRMAR_SENHA" id="senha2-profile" required />
         <div className="input-profile-password">
           <input type="submit" value="Cadastrar nova senha" />
         </div>

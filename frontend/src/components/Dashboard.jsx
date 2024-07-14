15e3172a5338dc2d5bce25f0050e0a93ecd33111
PedroHNunesAlves <pedrohenriquenunesalves067@gmail.com>
2024-07-13 21:57:54 -0300

    fix: docker-compose react env

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -16,7 +16,7 @@ const Dashboard = () => {
   const [page, setPage] = useState(0);
 
   // Route to list products
-  const url_fetchproducts = "http://backend:3000/produto/listar";
+  const url_fetchproducts = `${process.env.backend}/produto/listar`;
 
   // // // // // // // // // // // // // // // // // // // // // // // //
   // Set css to spinner loading
